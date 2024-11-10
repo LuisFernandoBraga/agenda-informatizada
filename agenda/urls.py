0dad680a8dfb59b959f193bc42eed242e013175c
agenda-informatizada <fernando_sorocaba_telefonica@hotmail.com>
2024-11-09 21:29:20 -0300

    atualizando

---
+++ agenda/urls.py
@@ -0,0 +1,19 @@
+# type: ignore
+from django.urls import path
+from agenda import views
+
+urlpatterns = [
+    path('pesquisa/', views.pesquisa, name='pesquisa'),
+    path('', views.index, name='index'),
+
+    # URLs para as páginas
+    path('agendamento/<int:cadastro_id>/detail/', views.cadastro, name='cadastro'),
+    path('agendamento/inserir_cadastro/', views.inserir_cadastro, name='inserir_cadastro'), 
+    path('agendamento/<int:cadastro_id>/atualiza/', views.atualiza, name='atualiza'),
+    path('agendamento/<int:cadastro_id>/excluir/', views.excluir, name='excluir'),
+    
+    # URL para user
+    path('usuario/criar/', views.registro, name='registro'),
+
+
+]
