1eee2cc1c3daca0dd5b8589933b499b162a79d71
agenda-informatizada <fernando_sorocaba_telefonica@hotmail.com>
2024-11-10 14:45:51 -0300

    Teste DB

--- project/settings.py
+++ project/settings.py
@@ -76,14 +76,25 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'agenda_informatizada',
+        'USER': 'luisfernando',
+        'PASSWORD': '32811880',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
